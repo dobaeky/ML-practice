5350fa5db2377fe1edbe927b21f6402608df0cbc
dobaeky <dyb1129@gmail.com>
2020-09-17 23:15:53 +0900

    initialize repo

---
+++ 123.py
@@ -0,0 +1,3 @@
+from test1 import add
+
+print(add(3,2))
---
+++ test1.py
@@ -0,0 +1,14 @@
+
+def add(int1 ,int2):
+    result = int1 + int2
+    return result
+
+arr = []
+if __name__ == "__main__":
+    for idx in range(1,10):
+        arr.append(idx)
+    print(arr)
+    print(len(arr))
+    #res = add('a','b')
+    #res1 = 5
+    #print(str(res1) + res + res)
